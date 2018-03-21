792af3306668e8ad3b0d99015ae9d4e66bf6a78f
Abdulmalik <abdulmalik.abdulwahab@andela.com>
2018-03-21 22:10:28 +0100

    Add function to get ticket fee

--- ticketingapp/models.py
+++ ticketingapp/models.py
@@ -1,5 +1,8 @@
+import math
+
 from django.db import models
 from django.core.validators import RegexValidator
+from django.utils import timezone
 
 # Create your models here.
 
@@ -22,10 +25,46 @@ class ParkingTicket(models.Model):
                                     validators=[plate_number_validator])
     entry_time = models.DateTimeField(auto_now_add=True)
     exit_time = models.DateTimeField(blank=True, null=True)
+    checkout_time = models.DateTimeField(blank=True, null=True)
     fee_paid = models.FloatField(default=0.0)
     status = models.CharField(choices=STATUS, default="parked", max_length=7)
     date_modified = models.DateTimeField(auto_now=True)
     mall = models.ForeignKey(Mall, related_name="mall", on_delete=models.CASCADE)
+
+    def get_ticket_fee(self):
+        THIRTY_MIN = 1800
+        ONE_HOUR = 3600
+        TWO_HOURS = 7200
+
+        stayed_time = timezone.now() - self.entry_time
+        stayed_time_seconds = stayed_time.total_seconds()
+
+        if stayed_time_seconds <= THIRTY_MIN:
+            return 0.0
+        elif stayed_time_seconds <= TWO_HOURS:
+            return math.ceil(stayed_time_seconds/ONE_HOUR) * 200
+        else:
+            return 400.0 + math.ceil((stayed_time_seconds - TWO_HOURS) / ONE_HOUR) * 100
+
+    def checkout(self):
+        if not self.checkout_time:
+            self.checkout_time = timezone.now()
+        return self.get_ticket_fee()
+
+    def amount_owed(self):
+        return self.get_ticket_fee() - self.fee_paid
+
+    def exit_park(self):
+        can_exit = self.amount_owed() <= 0
+        if can_exit:
+            self.status = 'exited'
+            self.exit_time = timezone.now()
+            self.save()
+        return can_exit
+    
+    def pay_ticket(self, amount):
+        self.fee_paid += amount
+        self.save()
 
     class Meta:
         indexes = [
