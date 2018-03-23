9769c60cd300b5f3c4e9117c06f365b0fb2739e7
Abdulmalik <abdulmalik.abdulwahab@andela.com>
2018-03-23 03:30:03 +0100

    Create a Proc file for Heroku

--- parkingsystem/settings/base.py
+++ parkingsystem/settings/base.py
@@ -76,17 +76,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'parkingsystem.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
