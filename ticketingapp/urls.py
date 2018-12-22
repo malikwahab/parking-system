f31eb29074c0522c9ec656dc7d725950970ef9a1
Malik <abdulmalik.abdulwahab@andela.com>
2018-12-23 00:05:25 +0100

    Add obtain jwt token url

--- ticketingapp/urls.py
+++ ticketingapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework import routers
 from rest_framework_nested.routers import NestedSimpleRouter
+from rest_framework_jwt.views import obtain_jwt_token
 
 from ticketingapp.views import (MallViewSet,
                                 ParkingTicketViewSet,
@@ -24,6 +25,9 @@ urlpatterns = [
     path('exit/<int:ticket_id>', exit_park, name='exit-route'),
     path('mall/<int:mall_id>/payment-details',
          payment_details, name='payment-details-route'),
+    path('auth-users', obtain_jwt_token),
+
+    *router.urls,
+    *mall_router.urls
 ]
 
-urlpatterns += [*router.urls, *mall_router.urls]
